1fdc50c7e956a0c23b712f2c2a056c28963919af
tornike440 <tokab98887@gmail.com>
2024-10-21 20:07:46 +0400

    commit

---
+++ junk/birthmonth.py
@@ -0,0 +1,16 @@
+name = input("what is your name?")
+
+birthmonth = input("which month were you born:")
+
+if name == "tornike" :
+    if birthmonth =="august":
+        print("what a coincidence!i have same name and same birthmonth as you!")
+    else:
+        print("oh, so we have the same name")
+if name == "demetre":
+    if birthmonth =="august":
+        print("what a coincidence!you have same name as my brother and same birthmonth as me!")
+    else:
+           print("oh, so you have same name as my brother")
+if name != "tornike" :
+    print("nice to meet you, my name is tornike")
---
+++ junk/inputtst.py
@@ -0,0 +1,12 @@
+name = input (" what is yout name ")
+age = input("how namy yo are you ")
+
+print("my name is " + name)
+
+print ("i am " + age + " years old")
+
+age2= int(age) +4
+
+print("in 2028 i will be ")
+print(age2)
+print(" years old")
