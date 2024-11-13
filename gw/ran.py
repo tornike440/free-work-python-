948672d46e761ce1f74f72f53f4a6bd6487c0f9e
tornike440 <tokab98887@gmail.com>
2024-11-13 22:06:03 +0400

    commit

---
+++ gw/ran.py
@@ -0,0 +1,13 @@
+import random
+
+# List of greetings
+greetings = [
+    "Hello there!",
+    "Hi, how are you?",
+    "Hey, what's up?",
+    "Good day!",
+    "Greetings, friend!"
+]
+
+# Select and print one random greeting
+print(random.choice(greetings))
